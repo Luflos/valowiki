7d0ed2bfc832b81b6ecea2619adb182df12926e3
clairxc <clairrrc@gmail.com>
2022-04-12 16:23:38 -0700

    more css

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -14,6 +14,9 @@ export default function Navbar() {
                     <li>
                         <Link to="/maps">Maps</Link>
                     </li>
+                    <li>
+                        <Link to="/weapons">Weapons</Link>
+                    </li>
                 </ul>
             </div>
         </nav>
