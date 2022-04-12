26f8b2fec8301e4700b662b8b270eabb594ddcfd
clairxc <clairrrc@gmail.com>
2022-04-12 16:20:39 -0700

    more css

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,15 +1,21 @@
+import { Link } from "react-router-dom"
+
 export default function Home() {
    return (
-      <div style={{ backgroundImage:`url("https://i.pinimg.com/564x/d3/39/5e/d3395ed9226a8a74c22891f4b2fe688f.jpg")`, backgroundRepeat:"no-repeat", backgroundPosition:"center", backgroundSize:"cover", height:"100%"}}>
-         <div style={{color:"white", border: "5px solid hotpink"}}>
-            <h1>Welcome to VALOwiki</h1>
+      <div className="homepage-bg">
+         <div className="home-div center">
+            <h1>Welcome to VALOWIKI</h1>
 
             <h3>Valorant is a free-to-play competitive 5v5 character-based tactical shooter game, developed by Riot Games. Set in a near-future Earth, you team up with four other players against five enemies in round-based combat with an agent of your choice. </h3>
 
             <h3>Valowiki is an app created for Valorant players who want to learn more about their favorite agents and also improve their gameplay.</h3>
+         </div>
 
-         <button className="button">buttonnnnn</button>
-         <button className="button">buttonnnnnn</button>
+         <div className="center">
+            <button className="idk"><Link to="/agents">Agents</Link></button>
+
+            <Link className="idk" to="/maps">Maps</Link>
+
          </div>
       </div>
    )
