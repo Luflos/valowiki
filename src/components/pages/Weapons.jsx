a8c51c3c11bd10b870d0ae4c6c8f5f989bf5c243
Sol Youn <syoun93@gmail.com>
2022-04-13 23:12:12 -0700

    seperated weapons by category

--- src/components/pages/Weapons.jsx
+++ src/components/pages/Weapons.jsx
@@ -2,27 +2,43 @@ import { Link } from "react-router-dom"
 import "../layout/Weapons.css"
 
 export default function Weapons({ weapons }) {
-   const weaponsList = weapons.map((weapon, idx) => {
-      return (
-         <div key={`weapon-${idx}`} className="center">
-            {weapon.displayName}
-            <br></br>
-            Category: {weapon.shopData === null ? "Melee" : weapon.shopData.category}
-            <p>
-               <Link to={`/weapons/${weapon.uuid}`}>
-                  <img src={weapon.displayIcon} alt={weapon.displayName} />
-               </Link>
-            </p>
-         </div>
-      )
-   })
+   // console.log(weapons)
+   const categories = new Map()
+
+   for (let index = 0; index < weapons.length; index++) {
+      const oneWeapon = weapons[index]
+
+      if (!categories.has(oneWeapon.category)) {
+         categories.set(oneWeapon.category, [])
+      }
+      categories.get(oneWeapon.category).push(oneWeapon)
+   }
+
+   const weaponsList = []
+   for (const arrayOfWeapons of categories.values()) {
+      const displayName = arrayOfWeapons[0].shopData?.category || "Melee"
+      weaponsList.push(<h2 className="center">{displayName}</h2>)
+
+      const weaponsGroup = arrayOfWeapons.map((weapon, idx) => {
+         return (
+            <div key={`weapon-${idx}`} className="center">
+               {weapon.displayName}
+               <br></br>
+               <p>
+                  <Link to={`/weapons/${weapon.uuid}`}>
+                     <img src={weapon.displayIcon} alt={weapon.displayName} />
+                  </Link>
+               </p>
+            </div>
+         )
+      })
+      weaponsList.push(<div className="grid-container">{weaponsGroup}</div>)
+   }
 
    return (
       <>
          <h1 className="center">List of Weapons</h1>
-      <div className="grid-container">
-         {weaponsList}
-      </div>
+         <div>{weaponsList}</div>
       </>
    )
 }
