77a17d468dc053bf3b6daabecd3bf3d7da0e6123
Sol Youn <syoun93@gmail.com>
2022-04-14 16:05:59 -0700

    hidden text

--- src/components/pages/Lineups.jsx
+++ src/components/pages/Lineups.jsx
@@ -1,20 +1,23 @@
-import {Link} from 'react-router-dom'
+import { Link } from "react-router-dom"
 
+export default function Lineups() {
+   return (
+      <div className="center">
+         <h1>Lineups</h1>
+         <div >
+            <h2>
+               Line ups are when you align your crosshair or other HUD (Heads up Display) elements
+               within the environment in order to shoot/throw an agent's ability to land in a
+               desired location. It is generally done while out of harms way to kil and/or displace
+               the enemy team to give you and your team an advantage.
+            </h2>
+         </div>
 
-
-
-export default function Lineups(){
-    return(
-        <div>
-            <div>
-                <h1>Lineups</h1>
-                <h4>Line ups are when you aligning your crosshair or other HUD (Heads up Display) elements within the environment in order to shoot/throw an agent's ability to land in a desired location. It is generally done while out of harms way to kill and/or displace the enemy team to give you and your team an advantage.</h4>
-            </div>
-
-            <div>
-                <Link to='/lineups/sova'><h1>Sova Lineups</h1></Link>
-                <Link to='/lineups/cypher'><h1>Cypher Setups</h1></Link>
-            </div>
-        </div>
-    )
+         <div style={{ margin: 30 }}>
+            <Link to="/lineups/sova">Sova Lineups</Link>
+            <br></br>
+            <Link to="/lineups/cypher">Cypher Setups</Link>
+         </div>
+      </div>
+   )
 }
