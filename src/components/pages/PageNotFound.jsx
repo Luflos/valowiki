8747c85552ef46dd7f4f84a2722b1962c3ab23d1
clairxc <clairrrc@gmail.com>
2022-04-15 16:25:09 -0700

    updated 404 gif

--- src/components/pages/PageNotFound.jsx
+++ src/components/pages/PageNotFound.jsx
@@ -4,8 +4,10 @@ export default function PageNotFound() {
     return (
         <div>
             <h1 style={{ textAlign: "center", paddingTop: "5rem" }}>404 Error: Page Not Found</h1>
-            <h1 style={{ textAlign: "center", fontSize: "200px"}}> ㅠ_ㅠ </h1>
-            <div className="center" style={{padding: "10rem"}}>
+            <div className="center">
+                <img src="/images/jett404.gif"/>
+            </div>
+            <div className="center" style={{padding: "5rem"}}>
                 <Link to="/">Go Back Home</Link>
             </div>
         </div>
