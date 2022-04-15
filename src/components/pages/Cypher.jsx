455e33b65b80cbae15f1b1c86df52b9bff873677
br1anlee <joonmolee3@gmail.com>
2022-04-15 10:22:23 -0700

    both line up pages are responsive

--- src/components/pages/Cypher.jsx
+++ src/components/pages/Cypher.jsx
@@ -1,76 +1,54 @@
-import ReactPlayer from "react-player"
-import "../layout/Lineups.css"
+import ReactPlayer from "react-player";
+import "../layout/Lineups.css";
 
 export default function Cypher() {
-   return (
+  return (
+    <>
       <div className="center">
-         <img
-            className="agent-imgs"
-            src="https://wallpx.com/image/2022/03/valorant-cypher-camera-agents-artwork.jpg"
-            alt="Cypher"
-         />
-
-         <div>
-            <h1 className="title">Split Cypher Setups</h1>
-
-            <h3 className="lineUpTitle">B Setup</h3>
-            <div>
-               <ReactPlayer controls url="https://youtu.be/_kgwebcpdxw" className='react-player'/>
-            </div>
-
-            <h3 className="lineUpTitle">A Setup</h3>
-            <div>
-               <ReactPlayer controls url="https://youtu.be/FCCmm8oKYWg" className='react-player' />
-            </div>
-
-            <h3 className="lineUpTitle">Early A Info Camera</h3>
-            <div>
-               <ReactPlayer controls url="https://youtu.be/VoGf5qwZL8Y" className='react-player'/>
-            </div>
-
-            <h3 className="lineUpTitle">A Camera</h3>
-            <div>
-               <ReactPlayer controls url="https://youtu.be/0L2XcIRMdSE" className='react-player'/>
-            </div>
-
-            <h3 className="lineUpTitle">A Camera #2</h3>
-            <div>
-               <ReactPlayer controls url="https://youtu.be/1_sar7p-IHk" className='react-player'/>
-            </div>
-
-            <div>
-               <h1 className="title">Bind Cypher Setups</h1>
-
-               <h3 className="lineUpTitle">B Setup</h3>
-               <div>
-                  <ReactPlayer controls url="https://youtu.be/k4vnsdNBEu8" className='react-player'/>
-               </div>
-
-               <h3 className="lineUpTitle">A Setup</h3>
-               <div>
-                  <ReactPlayer controls url="https://youtu.be/jHMsLL9HgOY" className='react-player'/>
-               </div>
-            </div>
-
-            <div>
-               <h1 className="title">Fracture Cypher Setups</h1>
-
-               <h3 className="lineUpTitle">B Setup</h3>
-               <div>
-                  <ReactPlayer controls url="https://youtu.be/TFteb9A_63c" className='react-player'/>
-               </div>
-
-               <h3 className="lineUpTitle">A Setup</h3>
-               <div>
-                  <ReactPlayer controls url="https://youtu.be/4xppZMixd0s" className='react-player'/>
-               </div>
-
-               <h3 className="lineUpTitle">B Main One Way</h3>
-               <div>
-                  <ReactPlayer controls url="https://youtu.be/T_e9Ocxrigk" className='react-player'/>
-               </div>
-            </div>
-         </div>
+        <img
+          className="agent-imgs"
+          src="https://wallpx.com/image/2022/03/valorant-cypher-camera-agents-artwork.jpg"
+          alt="Cypher"
+        />
       </div>
-   )
+      <h1 className="title center">Split Cypher Setups</h1>
+      <section className="layout">
+        <ReactPlayer
+          controls
+          url="https://youtu.be/_kgwebcpdxw"
+          className="react-player"
+        />
+
+        <ReactPlayer
+          controls
+          url="https://youtu.be/FCCmm8oKYWg"
+          className="react-player"
+        />
+
+        <ReactPlayer
+          controls
+          url="https://youtu.be/k4vnsdNBEu8"
+          className="react-player"
+        />
+
+        <ReactPlayer
+          controls
+          url="https://youtu.be/jHMsLL9HgOY"
+          className="react-player"
+        />
+
+        <ReactPlayer
+          controls
+          url="https://youtu.be/TFteb9A_63c"
+          className="react-player"
+        />
+
+        <ReactPlayer
+          controls
+          url="https://youtu.be/4xppZMixd0s"
+          className="react-player"
+        />
+      </section>
+    </>
+  );
 }
--- src/components/pages/Sova.jsx
+++ src/components/pages/Sova.jsx
@@ -3,59 +3,64 @@ import "../layout/Lineups.css"
 
 export default function Sova() {
    return (
-      <div className="center">
-         <img
-            className="agent-imgs"
-            src="https://wallpaperaccess.com/full/3779849.jpg"
-            alt="Sova"
-         />
-         <div>
-            <h2 className="title">Ascent Sova Line Ups</h2>
-
-            <h3 className="lineUpTitle">B Site to Mid</h3>
-            <div>
-               <ReactPlayer controls url="https://youtu.be/Dm_AnZbeamE" className='react-player'/>
-            </div>
-
-            <h3 className="lineUpTitle">A Hell to Heaven</h3>
-            <div>
-               <ReactPlayer controls url="https://www.youtube.com/watch?v=7lR2_FfKqUk" className='react-player'/>
-            </div>
-
-            <h3 className="lineUpTitle">CT to Boat</h3>
-            <div>
-               <ReactPlayer controls url="https://youtu.be/RCdzMhXYvn8" className='react-player'/>
-            </div>
-
-            <h3 className="lineUpTitle">CT Spawn to A</h3>
-            <div>
-               <ReactPlayer controls url="https://youtu.be/Csqb_JKoOwo" className='react-player'/>
-            </div>
+      <>
+         <div className="center">
+            <img 
+               className="agent-imgs"
+               src="https://wallpaperaccess.com/full/3779849.jpg"
+               alt="Sova pic"
+            />
          </div>
+         <h1 className="title center">Sova Line Ups</h1>
+         <section className="layout">
+            <ReactPlayer 
+            controls 
+            url="https://youtu.be/Dm_AnZbeamE" 
+            className='react-player'
+            />
 
-         <div>
-            <h2 className="title">Haven Sova Line Ups</h2>
+            <ReactPlayer 
+            controls 
+            url="https://www.youtube.com/watch?v=7lR2_FfKqUk" 
+            className='react-player'
+            />
 
-            <h3 className="lineUpTitle">Garage Recon</h3>
-            <div>
-               <ReactPlayer controls url="https://youtu.be/TR_OlrD8e_4" className='react-player'/>
-            </div>
+            <ReactPlayer 
+            controls 
+            url="https://youtu.be/RCdzMhXYvn8" 
+            className='react-player'
+            />
 
-            <h3 className="lineUpTitle">T Spawn To Garage</h3>
-            <div>
-               <ReactPlayer controls url="https://youtu.be/K9p7Hu9zSWU" className='react-player'/>
-            </div>
+            <ReactPlayer 
+            controls 
+            url="https://youtu.be/Csqb_JKoOwo" 
+            className='react-player'
+            />
 
-            <h3 className="lineUpTitle">C Main to C Site</h3>
-            <div>
-               <ReactPlayer controls url="https://youtu.be/bY5wwODz3S4" className='react-player'/>
-            </div>
+            <ReactPlayer 
+            controls 
+            url="https://youtu.be/TR_OlrD8e_4" 
+            className='react-player'
+            />
 
-            <h3 className="lineUpTitle">B Link to A Site</h3>
-            <div>
-               <ReactPlayer controls url="https://youtu.be/ENo5rg6PLmg" className='react-player'/>
-            </div>
-         </div>
-      </div>
+            <ReactPlayer 
+            controls 
+            url="https://youtu.be/K9p7Hu9zSWU" 
+            className='react-player'
+            />
+   
+            <ReactPlayer 
+            controls 
+            url="https://youtu.be/bY5wwODz3S4" 
+            className='react-player'
+            />
+            
+            <ReactPlayer 
+            controls 
+            url="https://youtu.be/ENo5rg6PLmg" 
+            className='react-player'
+            />
+         </section>
+      </>
    )
 }
