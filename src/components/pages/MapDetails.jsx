88331b9931ad1b1a400d8a4d2ef11d36c0bdb331
Sol Youn <syoun93@gmail.com>
2022-04-17 22:33:30 -0700

    fixed the range

--- src/components/pages/MapDetails.jsx
+++ src/components/pages/MapDetails.jsx
@@ -7,8 +7,9 @@ export default function MapDetails({ mapData }) {
             <h1 style={{marginBottom:20}}>{mapData.displayName}</h1>
             <div>
                <img className="map-imgs" src={mapData.splash} alt={mapData.displayName} />
-               {/* <img className= 'rotate' src={mapData.displayIcon} alt={mapData.displayName} /> */}
+               { mapData.displayName === 'The Range' ? '' : 
                <h2 style={{marginBottom:20, marginTop:20}} >Callouts for {mapData.displayName}</h2>
+               }
                { mapData.displayName === 'Ascent' ? <img className="map-imgs" src='../images/maps/ascent-map.png' alt={mapData.displayName} /> : '' }
                { mapData.displayName === 'Split' ? <img className="map-imgs" src='../images/maps/split-map.png' alt={mapData.displayName} /> : '' }
                { mapData.displayName === 'Fracture' ? <img className="map-imgs" src='../images/maps/fracture-map.png' alt={mapData.displayName} /> : '' }
